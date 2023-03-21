425c7388ab5ac019641c6b8b77b67bd7d380ed84
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-03-21 21:02:15 +0900

    テストフォルダに移動

--- test/Main.test.tsx
+++ test/Main.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Main from "../src/components/Main";
 
-test("renders learn react link", () => {
-  render(<App />);
+test("Mainのタイトルが正しいか", () => {
+  render(<Main />);
   const linkElement = screen.getByText("私の体重の遷移");
   expect(linkElement).toBeInTheDocument();
 });
