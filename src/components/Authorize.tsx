55c71bb1bc5cf77036f4ceb30e23d385f3df695a
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-03-21 21:02:27 +0900

    認可用画面を作成

---
+++ src/components/Authorize.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AuthorizeButton from "./AuthorizeButton";
+import Title from "./Title";
+
+const Authorize = () => {
+  return (
+    <>
+      <Title>認可を行なってください</Title>
+      <AuthorizeButton />;
+    </>
+  );
+};
+
+export default Authorize;
