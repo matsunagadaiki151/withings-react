3731265fdcfaf370e0d54a26404d3889afa852d0
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-03-21 21:03:19 +0900

    認可画面とメイン画面が切り替わるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import "./App.css";
-import AuthorizeButton from "./components/AuthorizeButton";
-import MyWeight from "./components/MyWeight";
-import Title from "./components/Title";
-import WeightChart from "./components/WeightChart";
+import Authorize from "./components/Authorize";
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <>
-      {<AuthorizeButton />}
-      <Title />
-      <MyWeight todayWeight={72.6} />
-      <WeightChart />
-    </>
-  );
+  const isAuthorized = false;
+  return <>{isAuthorized ? <Main /> : <Authorize />}</>;
 }
 
 export default App;
