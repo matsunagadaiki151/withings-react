f673fb8d870cd8b95f80fba38730250dde0bc3be
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-03-21 21:02:52 +0900

    タイトルをchidrenで変更できるよう修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Title = () => {
+type TitleProp = {
+  children: React.ReactNode;
+};
+
+const Title = (props: TitleProp) => {
   return (
     <header className="text-3xl text-gray-600 font-title font-bold text-center my-10 py-2">
-      私の体重の遷移
+      {props.children}
     </header>
   );
 };
