bf53b4f91b98d0097193a5692005933a726f27c9
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-03-31 19:21:27 +0900

    accessToken取得処理を追加し、refresh処理の途中まで追記

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Authorize from "./components/Authorize";
 import Main from "./components/Main";
@@ -7,17 +7,85 @@ import queryString from "query-string";
 import { useDispatch, Provider } from "react-redux";
 import { setEndpoint } from "./modules/actions";
 import { store } from "./modules/store";
+import type { Location } from "react-router-dom";
+import { Headers, Tokens } from "./type/types";
+import {
+  fetchTokenData,
+  loadAuthCodeFromQueryParams,
+  loadTokens,
+} from "./processor/tokenProcessor";
+import dayjs from "dayjs";
 import axios from "axios";
 
-type Tokens = {
-  accessToken: string;
+// 定数の定義
+const headers: Headers = {
+  "Content-Type": "application/json",
+};
+const DB_ENDPOINT = process.env.REACT_APP_DB_ENDPOINT;
+
+const renderComponent = async (location: Location) => {
+  const tokens = await loadTokens(DB_ENDPOINT, headers);
+  const newTokens = { ...tokens };
+  // クエリパラメータを取得する。
+  const queryParams = queryString.parse(location.search);
+
+  if (!tokens["accessToken"] && !queryParams["code"]) {
+    return <Authorize />;
+  } else if (!tokens["accessToken"]) {
+    const authCode = loadAuthCodeFromQueryParams(queryParams);
+    const params = {
+      code: authCode,
+    };
+    await fetchTokenData(newTokens, params, DB_ENDPOINT, headers);
+    return <div>アクセストークンを更新しました。</div>;
+  } else {
+    const limitTime = tokens["limitTime"];
+    if (dayjs(limitTime) <= dayjs()) {
+      const refreshTokenData = async () => {
+        const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
+        // const authCode = loadAuthCodeFromQueryParams(queryParams);
+        const refreshToken = tokens["refreshToken"];
+        const params = {
+          refresh_token: refreshToken,
+        };
+        const response = await axios.get(`${ENDPOINT}/refresh_token`, {
+          params: params,
+          headers: headers,
+        });
+
+        const data = response.data;
+        if (data["status"] !== 601) {
+          newTokens["accessToken"] = data["body"]["access_token"];
+          newTokens["refreshToken"] = data["body"]["refresh_token"];
+          const limitTime = dayjs().add(
+            parseInt(data["body"]["expires_in"]),
+            "second"
+          );
+          const limitTimeStr = limitTime.format("YYYY-MM-DD HH:mm:ss");
+          if (limitTimeStr !== "Invalid Date") {
+            newTokens["limitTime"] = limitTimeStr;
+          }
+          const postTokens = async (tokens: Tokens) => {
+            const DB_ENDPOINT = process.env.REACT_APP_DB_ENDPOINT;
+            await axios.post(`${DB_ENDPOINT}/tokens`, {
+              accessToken: tokens.accessToken,
+              refreshToken: tokens.refreshToken,
+              limitTime: tokens.limitTime,
+            });
+          };
+          postTokens(newTokens);
+        }
+      };
+      refreshTokenData();
+    }
+    return <Main />;
+  }
 };
 
 function MyComponent() {
   const dispatch = useDispatch();
-  const headers = {
-    "Content-Type": "application/json",
-  };
+  const [page, setPage] = useState(<>ロード中です。</>);
 
   useEffect(() => {
     const ENDPOINT = process.env.REACT_APP_ENDPOINT;
@@ -26,40 +94,15 @@ function MyComponent() {
 
   const location = useLocation();
 
-  const renderComponent = () => {
-    const tokens: Tokens = require("./data/tokens.json");
-    const queryParams = queryString.parse(location.search);
+  useEffect(() => {
+    const render = async () => {
+      const nowPage = await renderComponent(location);
+      setPage(nowPage);
+    };
+    render();
+  }, [location]);
 
-    if (!tokens["accessToken"] && !queryParams["code"]) {
-      return <Authorize />;
-    } else if (!tokens["accessToken"]) {
-      const authCode = queryParams["code"];
-
-      const params = {
-        code: authCode,
-      };
-
-      const fetchTokenData = async () => {
-        const ENDPOINT = process.env.REACT_APP_ENDPOINT;
-        const response = await axios.get(`${ENDPOINT}/get_token`, {
-          params: params,
-          headers: headers,
-        });
-
-        const data = response.data;
-        const accessToken = data["body"]["access_token"];
-        const refreshToken = data["body"]["refresh_token"];
-      };
-
-      fetchTokenData();
-
-      return <div>{authCode}</div>;
-    } else {
-      return <Main />;
-    }
-  };
-
-  return <>{renderComponent()}</>;
+  return <>{page}</>;
 }
 
 function App() {
