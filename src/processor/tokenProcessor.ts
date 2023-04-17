951110e6f951cc2027891326f7c52c2becd3e5cc
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-04-17 22:41:07 +0900

    体重取得処理を追加

--- src/processor/tokenProcessor.ts
+++ src/processor/tokenProcessor.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import queryString from "query-string";
 import { Tokens, Headers, AuthParams, RefreshParams } from "../type/types";
 
+const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
 export const loadTokens = async (
-  endpoint: string | undefined,
+  dbEndpoint: string | undefined,
   headers: Headers
 ): Promise<Tokens> => {
-  const response = await axios.get(`${endpoint}/tokens`, {
+  const response = await axios.get(`${dbEndpoint}/tokens`, {
     headers: headers,
   });
   return response.data;
@@ -24,8 +26,6 @@ export const loadAuthCodeFromQueryParams = (
 };
 
 export const fetchTokenData = async (params: AuthParams, headers: Headers) => {
-  const ENDPOINT = process.env.REACT_APP_ENDPOINT;
-
   const response = await axios.post(`${ENDPOINT}/get_token`, null, {
     params: params,
     headers: headers,
@@ -38,10 +38,28 @@ export const fetchRefreshTokenData = async (
   params: RefreshParams,
   headers: Headers
 ) => {
-  const ENDPOINT = process.env.REACT_APP_ENDPOINT;
-
-  await axios.post(`${ENDPOINT}/refresh_token`, {
+  await axios.post(`${ENDPOINT}/refresh_token`, null, {
     params: params,
     headers: headers,
   });
+};
+
+export const fetchWeightsData = async (
+  accessToken: string,
+  headers: Headers
+) => {
+  const params = {
+    access_token: accessToken,
+  };
+  const authHeader = {
+    ...headers,
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const measures = await axios.get(`${ENDPOINT}/load_measures`, {
+    params: params,
+    headers: authHeader,
+  });
+
+  return measures;
 };
