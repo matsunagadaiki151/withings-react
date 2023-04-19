85f70c802bd1bea927f588eb97bcd653c758bf19
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-04-19 21:41:41 +0900

    chart jsでグラフを描画

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,33 @@
 import dayjs from "dayjs";
 import { RootState } from "../modules/store";
 import MyWeight from "./MyWeight";
-import Title from "./Title";
-import WeightChart from "./WeightChart";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  TimeScale,
+  Legend,
+  LineElement,
+  Title as ChartTitle,
+  LinearScale,
+  PointElement,
+  Tooltip,
+} from "chart.js";
 import { useSelector } from "react-redux";
+import { Line } from "react-chartjs-2";
+import "chartjs-adapter-date-fns";
+
+import Title from "./Title";
+
+ChartJS.register(
+  CategoryScale,
+  TimeScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ChartTitle,
+  Tooltip,
+  Legend
+);
 
 const Main = () => {
   const weights = useSelector((state: RootState) => state.weights.weights);
@@ -17,11 +41,64 @@ const Main = () => {
     displayText = "今日の体重はまだ測られていません。";
   }
 
+  const sortedStrDates = Object.keys(weights).sort((a, b) =>
+    dayjs(a).diff(dayjs(b))
+  );
+  const sortedWeights = sortedStrDates.map((date) => weights[date]);
+  const sortedDates = sortedStrDates.map((date) => new Date(date));
+
+  const graphOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    tooltips: {
+      display: false,
+    },
+    scales: {
+      x: {
+        beginAtZero: true,
+        type: "time" as const,
+        time: { unit: "week" as const, displayFormats: { week: "yyyy/MM/dd" } },
+        ticks: {
+          font: {
+            size: 24,
+          },
+          maxRotation: 45,
+          minRotation: 45,
+        },
+      },
+      y: {
+        ticks: {
+          font: {
+            size: 24,
+          },
+        },
+      },
+    },
+  };
+
+  const graphData = {
+    labels: sortedDates,
+    datasets: [
+      {
+        label: "私の体重",
+        data: sortedWeights,
+        borderColor: "rgb(187, 201, 235)",
+      },
+    ],
+  };
+
   return (
     <>
       <Title>私の体重の遷移</Title>
       <MyWeight>{displayText}</MyWeight>
-      <WeightChart />
+      <div className="m-auto w-8/12 h-[800px]">
+        <Line options={graphOptions} data={graphData} />
+      </div>
     </>
   );
 };
